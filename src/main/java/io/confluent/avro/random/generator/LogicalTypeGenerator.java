6e5e589345ab7eb199b25bf45e38dd7301714d90
Eric <ericjoel.blancohermida@telefonica.com>
2020-09-29 11:53:41 +0200

    fix spaces

--- src/main/java/io/confluent/avro/random/generator/LogicalTypeGenerator.java
+++ src/main/java/io/confluent/avro/random/generator/LogicalTypeGenerator.java
@@ -116,100 +116,100 @@ public class LogicalTypeGenerator {
       case "iso8601-timestamp":
       case "datetime":
         Map dateRangeProps = Optional.ofNullable(propertiesProp.get(DATE_RANGE_PROP))
-                .map(m -> (Map) m)
-                .orElse(new HashMap());
+            .map(m -> (Map) m)
+            .orElse(new HashMap());
 
         java.sql.Timestamp isoDateTimeStart = Validations.tryParseDatetime((Optional.ofNullable(dateRangeProps.get(DATE_RANGE_PROP_START))
-                .map(Object::toString)
-                .orElse(DEFAULT_START_DATE_TIME)))
-                .getOrElse(() -> {
-                  throw new IllegalArgumentException(String.format(
-                          "Invalid iso date at field '%s' in %s property",
-                          DATE_RANGE_PROP_START,
-                          DATE_RANGE_PROP
-                  ));
-                });
+            .map(Object::toString)
+            .orElse(DEFAULT_START_DATE_TIME)))
+            .getOrElse(() -> {
+              throw new IllegalArgumentException(String.format(
+                  "Invalid iso date at field '%s' in %s property",
+                  DATE_RANGE_PROP_START,
+                  DATE_RANGE_PROP
+              ));
+            });
         java.sql.Timestamp isoDateTimeEnd = Validations.tryParseDatetime((Optional.ofNullable(dateRangeProps.get(DATE_RANGE_PROP_END))
-                .map(Object::toString)
-                .orElse(DEFAULT_END_DATE_TIME)))
-                .getOrElse(() -> {
-                  throw new IllegalArgumentException(String.format(
-                          "Invalid iso date at field '%s' in %s property",
-                          DEFAULT_END_DATE,
-                          DATE_RANGE_PROP
-                  ));
-                });
+            .map(Object::toString)
+            .orElse(DEFAULT_END_DATE_TIME)))
+            .getOrElse(() -> {
+              throw new IllegalArgumentException(String.format(
+                  "Invalid iso date at field '%s' in %s property",
+                  DEFAULT_END_DATE,
+                  DATE_RANGE_PROP
+              ));
+            });
 
         if (isoDateTimeStart.after(isoDateTimeEnd)) {
           throw new RuntimeException(String.format(
-                  "'%s' field must be strictly less than '%s' field in %s property",
-                  DATE_RANGE_PROP_START,
-                  DATE_RANGE_PROP_END,
-                  DATE_RANGE_PROP
+              "'%s' field must be strictly less than '%s' field in %s property",
+              DATE_RANGE_PROP_START,
+              DATE_RANGE_PROP_END,
+              DATE_RANGE_PROP
           ));
         }
         return ISO_LOCAL_DATE_TIME.format(dateBetween(isoDateTimeStart, isoDateTimeEnd)
-                .toInstant()
-                .atOffset(ZoneOffset.UTC));
+            .toInstant()
+            .atOffset(ZoneOffset.UTC));
       case "duration":
         Date durationStart = new Date();
         Date durationEnd = new Date(durationStart.getTime() + TimeUnit.DAYS.toMillis(MAX_DURATION_DAYS));
         return DurationFormatUtils.formatPeriodISO(durationStart.getTime(), dateBetween(durationStart, durationEnd).getTime());
       case "time":
         LocalDateTime time = LocalDateTime.of(LocalDate.now(),
-                LocalTime.of(
-                        random.nextInt(24), random.nextInt(60),
-                        random.nextInt(60), random.nextInt(999999999 + 1)
-                )
+            LocalTime.of(
+                random.nextInt(24), random.nextInt(60),
+                random.nextInt(60), random.nextInt(999999999 + 1)
+            )
         );
         return DateTimeFormatter.ISO_TIME.format(time);
       case "decimal-string":
         return String.format(Locale.US, "%.8f", random.nextDouble());
       case "phone-number":
         String regionCode = Optional.ofNullable(propertiesProp.get(REGION_CODE_PROP))
-                .map(Object::toString)
-                .orElseGet(() -> {
-                  List<String> regions = new ArrayList<>(phoneNumberUtil.getSupportedRegions());
-                  return regions.get(random.nextInt(regions.size()));
-                });
+            .map(Object::toString)
+            .orElseGet(() -> {
+              List<String> regions = new ArrayList<>(phoneNumberUtil.getSupportedRegions());
+              return regions.get(random.nextInt(regions.size()));
+            });
         return getRandomPhoneNumber(regionCode);
       case "iso-date":
         dateRangeProps = Optional.ofNullable(propertiesProp.get(DATE_RANGE_PROP))
-                .map(m -> (Map) m)
-                .orElse(new HashMap());
+            .map(m -> (Map) m)
+            .orElse(new HashMap());
 
         java.sql.Date isoDateStart = Validations.tryParseDate((Optional.ofNullable(dateRangeProps.get(DATE_RANGE_PROP_START))
-                .map(Object::toString)
-                .orElse(DEFAULT_START_DATE)))
-                .getOrElse(() -> {
-                  throw new IllegalArgumentException(String.format(
-                          "Invalid iso date at field '%s' in %s property",
-                          DATE_RANGE_PROP_START,
-                          DATE_RANGE_PROP
-                  ));
-                });
+            .map(Object::toString)
+            .orElse(DEFAULT_START_DATE)))
+            .getOrElse(() -> {
+              throw new IllegalArgumentException(String.format(
+                  "Invalid iso date at field '%s' in %s property",
+                  DATE_RANGE_PROP_START,
+                  DATE_RANGE_PROP
+              ));
+            });
         java.sql.Date isoDateEnd = Validations.tryParseDate((Optional.ofNullable(dateRangeProps.get(DATE_RANGE_PROP_END))
-                .map(Object::toString)
-                .orElse(DEFAULT_END_DATE)))
-                .getOrElse(() -> {
-                  throw new IllegalArgumentException(String.format(
-                          "Invalid iso date at field '%s' in %s property",
-                          DEFAULT_END_DATE,
-                          DATE_RANGE_PROP
-                  ));
-                });
+            .map(Object::toString)
+            .orElse(DEFAULT_END_DATE)))
+            .getOrElse(() -> {
+              throw new IllegalArgumentException(String.format(
+                  "Invalid iso date at field '%s' in %s property",
+                  DEFAULT_END_DATE,
+                  DATE_RANGE_PROP
+              ));
+            });
 
         if (isoDateStart.after(isoDateEnd)) {
           throw new RuntimeException(String.format(
-                  "'%s' field must be strictly less than '%s' field in %s property",
-                  DATE_RANGE_PROP_START,
-                  DATE_RANGE_PROP_END,
-                  DATE_RANGE_PROP
+              "'%s' field must be strictly less than '%s' field in %s property",
+              DATE_RANGE_PROP_START,
+              DATE_RANGE_PROP_END,
+              DATE_RANGE_PROP
           ));
         }
         return ISO_LOCAL_DATE.format(dateBetween(isoDateStart, isoDateEnd)
-                .toInstant()
-                .atOffset(ZoneOffset.UTC));
+            .toInstant()
+            .atOffset(ZoneOffset.UTC));
       case "country-code-numeric":
         List<String> numericCodes = JavaConverters.seqAsJavaList(Validations.countryCodeNumeric());
         return numericCodes.get(random.nextInt(numericCodes.size()));
@@ -224,10 +224,10 @@ public class LogicalTypeGenerator {
         return alphaCurrency.get(random.nextInt(alphaCurrency.size()));
       case "currency-code-numeric":
         List<Object> numericCurrencyCode = JavaConverters
-                .seqAsJavaList(Validations.currencyCodeNumeric().toSeq());
+            .seqAsJavaList(Validations.currencyCodeNumeric().toSeq());
         int currencyCode = Integer.parseInt(numericCurrencyCode
-                .get(random.nextInt(numericCurrencyCode.size()))
-                .toString());
+            .get(random.nextInt(numericCurrencyCode.size()))
+            .toString());
         return String.format("%03d", currencyCode);
       case "imei":
         return digitsGenerator.generate(LogicalTypeGenerator.IMEI_LENGTH);
