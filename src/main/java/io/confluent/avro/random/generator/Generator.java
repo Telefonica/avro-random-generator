427816cd7eb01df3df64b09af67e64e469788971
Carlos del Prado <carlos.delprado@11paths.com>
2020-11-18 15:40:05 +0000

    fix: double comparator

--- src/main/java/io/confluent/avro/random/generator/Generator.java
+++ src/main/java/io/confluent/avro/random/generator/Generator.java
@@ -1520,7 +1520,7 @@ public class Generator {
         wholeProb += typeProb;
       }
 
-      if (wholeProb.compareTo(1.0) != 0) {
+      if (Math.abs(1.0 - wholeProb) < Double.MIN_NORMAL) {
         throw new RuntimeException(String.format(
             "all probabilities of %s property must sum 1 at %s: %f",
             DISTRIBUTION_PROP,
